d0bef0fc2dc762b31e739dce275136ae60953494
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-02-07 09:49:33 -0500

    addeding demographics table and inserting

--- gender_assignment.rb
+++ gender_assignment.rb
@@ -1,12 +1,31 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'pry'
 require 'sequel'
 require 'sexmachine'
 
+# Connect db
+DB = Sequel.connect(:adapter=>'postgres', :host=>'localhost', :database=>'machine_learning', :user=>'rails')
+
+# Setup table to hold data
+DB.drop_table(:user_demographics)
+DB.create_table(:user_demographics) do
+  foreign_key :user_id
+  String :first_name
+  String :assigned_gender
+  String :real_gender
+end
+
+# Load detector
 d = SexMachine::Detector.new(:case_sensitive => false)
 
-DB = Sequel.connect(:adapter=>'postgres', :host=>'localhost', :database=>'machine_learning', :user=>'rails')
+# Assign users
 users = DB[:users]
+demo = DB[:user_demographics]
+
 users.each do |user|
-  puts "#{user[:first_name]}, #{d.get_gender user[:first_name]}"
+  name = user[:first_name]
+  gender = d.get_gender user[:first_name]
+  demo.insert(user_id: user[:id], first_name: name, assigned_gender: gender.to_s)
+  puts "#{name}, #{gender}"
 end
