9960e70e237a377e398afca025fc373c00d86981
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-02-07 16:31:46 -0500

    refactored clustering

--- clustering/clustering.rb
+++ clustering/clustering.rb
@@ -90,13 +90,16 @@ def kmeans
            'java' => '9', 'agile' => '8' }
 
   # Define our two clusters and initialize them with two users
-  cluster1 = Cluster.new
-  cluster1.add(marc)
-  cluster1.add(megan)
+  clusters = []
+  cluster =  Cluster.new
+  cluster.add(marc)
+  cluster.add(megan)
+  clusters << cluster
 
-  cluster2 = Cluster.new
-  cluster2.add(elise)
-  cluster2.add(jill)
+  cluster = Cluster.new
+  cluster.add(elise)
+  cluster.add(jill)
+  clusters << cluster
 
   changed = true
 
@@ -106,70 +109,58 @@ def kmeans
     changed = false
 
     # Recalculate each cluster's centroid (mean)
-    cluster1.calculate_centroid
-    cluster2.calculate_centroid
+    clusters.each_with_index do |cluster, i|
+      cluster.calculate_centroid
 
-    # Get the membership of each cluster
-    people1 = cluster1.get_people
-    people2 = cluster2.get_people
+      # Get the membership of each cluster
+      people = cluster.get_people
 
-    # Check members of cluster 1 against the cluster centroids
-    puts "Checking cluster 1"
-    people1.each do |person|
-      if cluster2.calculate_gd(person) < cluster1.calculate_gd(person) then
-        puts "Feature vector moved from cluster 1 to cluster 2"
-        cluster1.remove(person)
-        cluster2.add(person)
-        changed = true
+      # Check members of cluster 1 against the cluster centroids
+      puts "Checking cluster #{i}"
+      people.each do |person|
+        clusters.each_with_index do |other_cluster, j|
+          if other_cluster.calculate_gd(person) < cluster.calculate_gd(person) then
+            puts "Feature vector moved from cluster #{i} to cluster #{j}"
+            cluster.remove(person)
+            other_cluster.add(person)
+            changed = true
+          end
+        end
       end
-    end
 
-    # Check members of cluster 2 against the cluster centroids
-    puts "Checking cluster 2"
-    people2.each do |person|
-      if cluster1.calculate_gd(person) < cluster2.calculate_gd(person) then
-        puts "Feature vector moved from cluster 2 to cluster 1"
-        cluster2.remove(person)
-        cluster1.add(person)
-        changed = true
-      end
     end
-
   end
 
   puts
 
-  puts "Cluster 1 contains"
-  people = cluster1.get_people
-  people.each do |person|
-    person.each do |key,value| print "#{key}=#{value} " end
+  clusters.each_with_index do |cluster, i|
+    puts "Cluster #{i} contains"
+    people = cluster.get_people
+    people.each do |person|
+      person.each do |key,value| print "#{key}=#{value} " end
+      puts
+    end
     puts
   end
-  puts
-
-  puts "Cluster 2 contains"
-  people = cluster2.get_people
-  people.each do |person|
-    person.each do |key,value| print "#{key}=#{value} " end
-    puts
-  end
-  puts
 
 end
 
 kmeans
 
-# Checking cluster 1
-# Feature vector moved from cluster 1 to cluster 2
-# Checking cluster 2
-# Feature vector moved from cluster 2 to cluster 1
-# Checking cluster 1
-# Checking cluster 2
+puts <<EOF
+================================================
+Checking cluster 1
+Feature vector moved from cluster 1 to cluster 2
+Checking cluster 2
+Feature vector moved from cluster 2 to cluster 1
+Checking cluster 1
+Checking cluster 2
 
-# Cluster 1 contains
-# linux=13 oss=10 cloud=6 java=0 agile=0
-# linux=11 oss=0 cloud=9 java=0 agile=1
+Cluster 1 contains
+linux=13 oss=10 cloud=6 java=0 agile=0
+linux=11 oss=0 cloud=9 java=0 agile=1
 
-# Cluster 2 contains
-# linux=0 oss=3 cloud=0 java=9 agile=8
-# linux=3 oss=0 cloud=1 java=6 agile=7
+Cluster 2 contains
+linux=0 oss=3 cloud=0 java=9 agile=8
+linux=3 oss=0 cloud=1 java=6 agile=7
+EOF
