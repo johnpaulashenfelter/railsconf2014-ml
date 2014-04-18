e8b2d54d9fc81d7da624dc525b28fdd5a2e11da8
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-04-18 14:00:32 -0400

    added results table to db and code tweaks

--- ex2_geolocation/assign_location_to_users.rb
+++ ex2_geolocation/assign_location_to_users.rb
@@ -1,5 +1,3 @@
-#cd $GOPATH/src/github.com/fiorix/freegeoip
-# ./freegeoip
 require 'rubygems'
 require 'bundler/setup'
 require 'pry'
@@ -8,16 +6,7 @@ require 'faraday'
 require 'json'
 
 # Connect db
-# pg DB = Sequel.connect(adapter: 'postgres', host: 'localhost', database: 'machine_learning', user: 'rails')
 DB = Sequel.connect('sqlite://machine-learning.db')
-
-GEOCODER = 'http://127.0.0.1:8080'
-
-conn = Faraday.new(url: GEOCODER) do |faraday|
-  faraday.request  :url_encoded             # form-encode POST params
-  # faraday.response :logger                  # log requests to STDOUT
-  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-end
 
 # Setup table to hold data
 DB.create_table!(:location_demographics) do
@@ -38,13 +27,23 @@ end
 users = DB[:users]
 demo = DB[:location_demographics]
 
+
+# Setup local freegeoip server
+# cd $GOPATH/src/github.com/fiorix/freegeoip
+# ./freegeoip
+GEOCODER = 'http://127.0.0.1:8080'
+
+# Connect to local freegeoip server
+conn = Faraday.new(url: GEOCODER) do |faraday|
+  faraday.request  :url_encoded             # form-encode POST params
+  faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+end
+
 users.each do |user|
-  name = user[:first_name]
   if user[:current_login_ip].match(Resolv::IPv4::Regex)
     json = conn.get("/json/#{user[:current_login_ip]}").body
     geodata = JSON.parse(json)
-    puts "#{name}, #{geodata.inspect}"
+    puts "#{user[:first_name]}, #{geodata.inspect}"
     demo.insert(user_id: user[:id], lat: geodata["latitude"], lng: geodata["longitude"], country_code: geodata["country_name"], country_name: geodata["country_name"], region_code: geodata["region_code"], region_name: geodata["region_name"], city: geodata["city"], metro_code: geodata["metro_code"], areacode: geodata["areacode"], location_json: json)
   end
 end
-binding.pry
