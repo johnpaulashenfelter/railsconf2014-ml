5116160e30c6c3950cec0cacd62643ee780fdaad
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2014-04-17 20:57:37 -0400

    better docs, my data

--- ex4_similarity/similar_users.rb
+++ ex4_similarity/similar_users.rb
@@ -8,16 +8,25 @@ require 'sequel'
 #DB = Sequel.connect('postgres://localhost/machine_learning?user=rails')
 DB = Sequel.connect('sqlite://machine-learning.db')
 
-users_list = DB[:users].limit(10)
+users_list = DB[:users].all #limit(100)
 
 answers = []
 users = {}
 cols = %w(html css js ruby ios biz android php wp design devtools)
+rejects = []
 
 users_list.each_with_index do |user,i|
-  users[i] = "#{user[:first_name]} #{user[:last_name]}"
-  answers << [user[:html_points], user[:css_points], user[:js_points], user[:ruby_points], user[:ios_points], user[:biz_points], user[:android_points], user[:php_points], user[:wp_points], user[:design_points], user[:devtools_points]]
+  points = [user[:html_points], user[:css_points], user[:js_points], user[:ruby_points], user[:ios_points], user[:biz_points], user[:android_points], user[:php_points], user[:wp_points], user[:design_points], user[:devtools_points]]
+  if points.inject(&:+) > 0
+    users[i] = "#{user[:first_name]} #{user[:last_name]}"
+    answers << points
+  else
+    rejects << "{user[:first_name]} #{user[:last_name]}"
+  end
 end
+
+puts "Rejected #{rejects.count} records for 0 earned points"
+puts "Using #{users.count} users for recommendations"
 
 m = Linalg::DMatrix.columns(answers)
 # Compute the SVD Decomposition
@@ -33,8 +42,11 @@ v2 = Linalg::DMatrix.join_columns [vt.column(0), vt.column(1)]
 eig2 = Linalg::DMatrix.columns [s.column(0).to_a.flatten[0,2], s.column(1).to_a.flatten[0,2]]
 
 # Here comes Bob, our new user
-user = DB[:users].where('id = 111716').first
-a=[user[:html_points], user[:css_points], user[:js_points], user[:ruby_points], user[:ios_points], user[:biz_points], user[:android_points], user[:php_points], user[:wp_points], user[:design_points], user[:devtools_points]]
+#user = DB[:users].where('id = 111716').first
+#a=[user[:html_points], user[:css_points], user[:js_points], user[:ruby_points], user[:ios_points], user[:biz_points], user[:android_points], user[:php_points], user[:wp_points], user[:design_points], user[:devtools_points]]
+
+a=[7,0,0,94,160,7,1,19,0,0,19]
+puts "#{a} Test User (Bob)"
 
 bob = Linalg::DMatrix[a]
 bobEmbed = bob * u2 * eig2.inverse
@@ -46,9 +58,18 @@ v2.rows.each { |x|
     count += 1
   }
 
+threshold = 0.98
+puts "Filtering all users less than #{threshold} cosine similarity"
+
 # Remove all users who fall below the 0.90 cosine similarity cutoff and sort by similarity
-similar_users = user_sim.delete_if {|k,sim| sim < 0.9 }.sort {|a,b| b[1] <=> a[1] }
-similar_users.each { |u| printf "%s (ID: %d, Similarity: %0.3f) \n", users[u[0]], u[0], u[1]  }
+similar_users = user_sim.delete_if {|k,sim| sim < threshold }.sort {|a,b| b[1] <=> a[1] }
+
+puts "Found #{similar_users.count} similar users"
+
+similar_users.each do |u|
+  printf "%s %s (ID: %d, Similarity: %0.3f) \n", answers[u[0]], users[u[0]], u[0], u[1]
+end
+
 
 # We'll use a simple strategy in this case:
 #   1) Select the most similar user
